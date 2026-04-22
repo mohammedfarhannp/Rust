0981b226078945b4090ce386a946ff670fd1cd8a
mohammedfarhannp <m.farhan.np@gmail.com>
2026-04-22 18:09:05 +0530

    Variables.rs

---
+++ Variables.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let x: i32 = 500; // Variable Initialized and used
+    let y: i32; // Variable Initialized but not used // Spits Warning
+    
+    println!("The Value of x is {}", x);
+}
