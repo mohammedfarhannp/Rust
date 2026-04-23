2c2b063e6414bf11ed21f451780536d281c321d4
mohammedfarhannp <m.farhan.np@gmail.com>
2026-04-23 15:36:15 +0530

    Destructure tuple, similar to python unpacking tuple to variables.

--- Variables.rs
+++ Variables.rs
@@ -1,3 +1,6 @@
+#[allow(unused_variables)]
+#[allow(unused_assignments)]
+
 fn main() {
     let x: i32 = 500; // Variable Initialized and used // Spits warning if not used
     let y: i32; // Variable Initialized but not used // Spits Warning
@@ -8,5 +11,12 @@ fn main() {
     
     a += 1;
 
-    println!("The Value of x is {}", a);
+    // Destructuring a tuple data structure
+    let i: i8;
+    let j: i8;
+    let k: i16;
+    
+    (i, j, k) = (10, 20, 200);
+
+    println!("The Value of i, j, k are {}, {}, {} respectively", i, j, k);
 }
