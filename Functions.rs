4e2164500ff561d4d8dee5df6bf0ad7fa7389710
mohammedfarhannp <m.farhan.np@gmail.com>
2026-04-25 20:19:57 +0530

    Function Examples...

--- Functions.rs
+++ Functions.rs
@@ -1,8 +1,16 @@
 fn main() {
-    example();
+    let x: i8 = 20;
+    let y: i8 = 30;
+    
+    println!("{}", add(x, y));
 }
 
+#[allow(dead_code)]
 fn example() {
     let x: &str = "Hello";
     println!("{} World", x);
+}
+
+fn add(a: i8, b: i8) -> i8 {
+    return a + b;
 }
