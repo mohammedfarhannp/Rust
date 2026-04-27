cb64984148e841e832020460f4e38280a29a3506
mohammedfarhannp <m.farhan.np@gmail.com>
2026-04-27 18:11:25 +0530

    String Pointers!

--- Pointers.rs
+++ Pointers.rs
@@ -1,7 +1,11 @@
 fn main() {
     
     let s1 = String::from("Hello World");
-    let s2 = s1.clone();
+    let s2 = s1.clone(); // Copys or clones everything s1 has.
+    
+    
+    /* let s2 = s1 */
+    // Can't do this to copy, here s1 is no longer usable as data in rust can only have one owner
     
     println!("String 1 = {}\nString 2 = {}", s1, s2);
 }
