5b19295809ab91c74a6ff7139b5b5bd708dfba02
mohammedfarhannp <m.farhan.np@gmail.com>
2026-04-29 17:59:05 +0530

    User Inputs

--- user_inputs.rs
+++ user_inputs.rs
@@ -4,11 +4,29 @@ use std::io::Write; // For flushing...
 #[allow(unused_assignments)]
 #[allow(unused_variables)]
 fn main() {
-    let s: String = string_input("What's you name: ");
-    println!("Welcome back Mr. {}", s);    
+    //let s: String = string_input("What's you name: ");
+    //println!("Welcome back Mr. {}", s);    
+
+    let x: i32 = input_i32("Enter a Number: ");
+    println!("Out: {}", x);
+    
+    
 }
 
-// Function for user input (string pointer)
+fn input_i32(prompt: &str) -> i32 {
+    print!("{}", prompt);
+    io::stdout().flush().expect("Failed to flush");
+    
+    let mut input = String::new(); // pointer string type
+    
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+    
+    input.trim().parse().expect("Enter a valid integer")
+    
+}
+
+
+#[allow(dead_code)]
 fn string_input(prompt: &str) -> String {
     print!("{}", prompt);
     io::stdout().flush().expect("Failed to flush");
