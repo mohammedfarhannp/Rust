248dc7e7f1ed6b8e323954cc58910eb7638c69b4
mohammedfarhannp <m.farhan.np@gmail.com>
2026-05-02 19:55:46 +0530

    Checkpoint Save!

--- Palindrome_Str.rs
+++ Palindrome_Str.rs
@@ -2,15 +2,29 @@ use std::io;
 use std::io::Write;
 
 fn main() {
-    let str_1 : &str = str_input("Enter a String: ");
-    if is_palindrome(str_1) {
-        println!("{} is palindrome!", str_1);
-    } else {
-        println!("{} is not palindrome!", str_1);
-    }
+    let str_1 : String = str_input("Enter a String: ");
+    is_palindrome(String);
 }
 
-fn str_input(prompt: &str) -> &str {
+fn str_input(prompt: &str) -> String {
     print!("{}", prompt);
-    
+    io::stdout().flush().expect("Flush Failure!");
+
+    let mut input : String = String::new();
+
+    io::stdin().read_line(&mut input).expect("Read Failure!");
+    input.trim().to_string()
+}
+
+fn is_palindrome(string : &str) -> bool {
+
+    let mut rev : String = String::new();
+    let mut length : u8 = string.chars().count()
+
+    for i in 0..length {
+        
+    }
+
+    println!("{}", rev);
+    false
 }
